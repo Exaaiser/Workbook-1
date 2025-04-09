d5cfb951907469a16e32ca1593801408d428f97a
Exaaiser <umutheturk@gmail.com>
2025-04-09 11:44:14 -0400

    Complated ShoppingList App

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Pasta, Egg, Coca cola, Chicken Breast, Coffee, Tea, Milk, Protein Bar");
+    }
+}
