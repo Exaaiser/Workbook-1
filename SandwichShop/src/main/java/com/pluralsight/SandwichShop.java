3bbffe69b0c83b1ce5830c37292f4f13e947a765
Exaaiser <umutheturk@gmail.com>
2025-04-11 14:24:08 -0400

    Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -11,14 +11,36 @@ public class SandwichShop {
         double regular = 5.45;
         double large = 8.95;
         double price = 0;
+        String sandwichSize = "";
 
         if (string.equals("large")) {
             System.out.println("Large Sandwich is 8.95");
             price = large;
+            sandwichSize = "Large";
         if (string.equals("regular")) {
             System.out.println("Regular Sandwich is 5.45");
             price = regular;
+            sandwichSize = "Regular";
         }
+
+            System.out.println("Would you like the sandwich 'loaded' (YES/no)?");
+            System.out.println("- Regular: $1.00");
+            System.out.println("- Large: $1.75");
+            String loadedInput = keyboard.nextLine();
+            if (loadedInput.equals("YES")) {
+                if (sandwichSize.equals("Regular")) {
+                    price += 1.00; // Regular için $1.00 ekle
+                    System.out.println("Added $1.00 for a loaded Regular sandwich.");
+                } else if (sandwichSize.equals("Large")) {
+                    price += 1.75; // Large için $1.75 ekle
+                    System.out.println("Added $1.75 for a loaded Large sandwich.");
+                }
+            } else {
+                System.out.println("No additional cost for a non-loaded sandwich.");
+            }
+
+
+            }
 
     System.out.println("Whats your age?");
     int age = keyboard.nextInt();
@@ -32,4 +54,4 @@ public class SandwichShop {
     }
             System.out.println("Total price after discount: " + price);
         }
-    } }
+    }
