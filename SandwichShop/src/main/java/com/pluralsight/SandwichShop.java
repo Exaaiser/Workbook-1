5159cd24a8cd5f95ea59e13fc6485b7355ec1626
Exaaiser <umutheturk@gmail.com>
2025-04-11 11:58:37 -0400

    Completed Mod4 - Exercise 1 - Sandwich Shop

---
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Choose Portion");
+        String string = keyboard.nextLine();
+        double regular = 5.45;
+        double large = 8.95;
+
+        if (string.equals("large")) {
+            System.out.println("Large Sandwich is 8.95");
+        if (string.equals("regular")) {
+            System.out.println("Regular Sandwich is 5.45");
+
+    System.out.println("Whats your age?");
+    int age = keyboard.nextInt();
+
+    System.out.println("Senior");
+    System.out.println("Young");
+
+
+
+
+
+
+
+
+
+        }
+ } } }
