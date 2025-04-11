1ee8e126199e4f615d033ef25192edfa3582543b
Exaaiser <umutheturk@gmail.com>
2025-04-11 13:21:23 -0400

    Completed Mod4 - Exercise 1 - Sandwich Shop

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -10,25 +10,26 @@ public class SandwichShop {
         String string = keyboard.nextLine();
         double regular = 5.45;
         double large = 8.95;
+        double price = 0;
 
         if (string.equals("large")) {
             System.out.println("Large Sandwich is 8.95");
+            price = large;
         if (string.equals("regular")) {
             System.out.println("Regular Sandwich is 5.45");
+            price = regular;
+        }
 
     System.out.println("Whats your age?");
     int age = keyboard.nextInt();
 
-    System.out.println("Senior");
-    System.out.println("Young");
-
-
-
-
-
-
-
-
-
+    if (age <= 17) {
+        System.out.println("Senior, you qualify the discount");
+    price *= 0.9;
+    } else if (age >= 65) {
+        System.out.println("You qualify for a senior discount!");
+        price *= 0.80;
+    }
+            System.out.println("Total price after discount: " + price);
         }
- } } }
+    } }
